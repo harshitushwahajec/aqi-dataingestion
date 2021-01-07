a5241d6432972a7a7edc51ae30d40c7d32b3da21
Harshit Kushwaha <hkushwaha6@gmail.com>
2021-01-07 20:26:47 +0530

    service name removed

--- src/test/java/com/aqiindia/datasource/DataingestApplicationTests.java
+++ src/test/java/com/aqiindia/datasource/DataingestApplicationTests.java
@@ -3,10 +3,10 @@ package com.aqiindia.datasource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DataingestApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
