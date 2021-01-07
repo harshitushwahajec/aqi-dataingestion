53d7c7ea3f6ea7617eed88940a4cfc23cfff0c8c
Harshit Kushwaha <hkushwaha6@gmail.com>
2021-01-07 18:41:41 +0530

    update

--- src/main/java/com/aqiindia/datasource/controller/AqiDataIngestionController.java
+++ src/main/java/com/aqiindia/datasource/controller/AqiDataIngestionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("notvalidanymore")
+@RequestMapping("dataIngestion")
 public class AqiDataIngestionController {
 
     @Autowired
